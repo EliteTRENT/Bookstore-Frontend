58feca29f1ffa54401ae0a88c7ce9b626fe7bbc6
Aryan Negi <negiaryan2803@gmail.com>
2025-04-07 22:58:10 +0530

    [Aryan] MOD: Changed env.js path

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const path = require('path');
 const env = {
   GOOGLE_CLIENT_ID: "892883759524-crgr5ag4eu4o21c1ginihfbsouhm9u1v.apps.googleusercontent.com",
   GITHUB_CLIENT_ID: "Ov23li6FLQ78HLhxZa0y",
-  BACKEND_URL: process.env.BACKEND_URL || 'http://127.0.0.1:3000'
+  BACKEND_URL: process.env.BACKEND_URL
 };
 
 const content = `window.env = ${JSON.stringify(env, null, 2)};`;
