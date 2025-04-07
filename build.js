b441793038802a2ea0d4820c3062304b7f544f97
Aryan Negi <negiaryan2803@gmail.com>
2025-04-07 22:44:51 +0530

    [Aryan] ADD: Added build.js

---
+++ build.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const env = {
+  GOOGLE_CLIENT_ID: "892883759524-crgr5ag4eu4o21c1ginihfbsouhm9u1v.apps.googleusercontent.com",
+  GITHUB_CLIENT_ID: "Ov23li6FLQ78HLhxZa0y",
+  BACKEND_URL: process.env.BACKEND_URL || 'http://127.0.0.1:3000'
+};
+
+const content = `window.env = ${JSON.stringify(env, null, 2)};`;
+fs.writeFileSync(path.join(__dirname, 'js', 'env.js'), content);
+console.log('Generated env.js');
